bc6b2fbd61ddfaaa46e4ab1c15bbb4e374cb2690
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-28 23:19:12 +0600

    add feature to import missing states to a csv

--- main.py
+++ main.py
@@ -20,22 +20,43 @@ user_answer = screen.textinput(title="Guess the State", prompt="What's the state
 correct_answer = []
 user_score = 0
 end_game = False
+
 while not end_game:
-    for each_state in all_states.state:
-        if user_answer == each_state:
-            if user_answer in correct_answer:
-                pass
-            else:
-                state = all_states[all_states.state == user_answer]
-                states_name.goto(state.x.values[0], state.y.values[0])
-                states_name.write(user_answer, align="center", font=("raleway", 6, "bold"))
-                correct_answer.append(user_answer)
-                user_score += 1
-    if len(correct_answer) == 50:
-        end_game = True
+    if user_answer == "Exit":
+        break
     else:
-        user_answer = screen.textinput(title=f"{user_score}/50 States Correct",
-                                       prompt="What's another state?").title()
+        for each_state in all_states.state:
+            if user_answer == each_state:
+                if user_answer in correct_answer:
+                    pass
+                else:
+                    state = all_states[all_states.state == user_answer]
+                    states_name.goto(state.x.values[0], state.y.values[0])
+                    states_name.write(user_answer, align="center", font=("raleway", 6, "bold"))
+                    correct_answer.append(user_answer)
+                    user_score += 1
+
+        if len(correct_answer) == 50:
+            end_game = True
+        else:
+            user_answer = screen.textinput(title=f"{user_score}/50 States Correct",
+                                           prompt="What's another state?").title()
+
 
 # Screen exit
 screen.exitonclick()
+
+# Generate a CSV file with all missing states
+missing_states = []
+for each_state in all_states.state:
+    if each_state in correct_answer:
+        pass
+    else:
+        missing_states.append(each_state)
+
+df_missing_states = {
+    "state": missing_states,
+}
+
+pd.DataFrame(df_missing_states).to_csv("states_to_learn.csv")
+
